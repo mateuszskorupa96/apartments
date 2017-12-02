cb5c18fa52e10a470258c759a249d0b0433f36ce
mateuszskorupa96 <mateusz2996@interia.pl>
2017-12-02 17:46:19 +0100

    init commit

--- src/main/java/com/mateusz/apartmentstore/repositories/ApartmentRepository.java
+++ src/main/java/com/mateusz/apartmentstore/repositories/ApartmentRepository.java
@@ -2,7 +2,6 @@ package com.mateusz.apartmentstore.repositories;
 
 import com.mateusz.apartmentstore.model.Apartment;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface ApartmentRepository extends CrudRepository<Apartment, Long> {
